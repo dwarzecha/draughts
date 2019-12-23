3b22ba3673e5b8b48f2ecf6ccb6c5268ccc83bd9
Simply27 <dawid.warzecha@onet.pl>
2019-12-23 21:31:56 +0100

    fill_men better, but far from perfect

--- main.c
+++ main.c
@@ -1,19 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-// To jest zmiana
+#include <stdbool.h>
 
 /*
 TODO
+- fill men function 
 - print men function
 - choosing game type?
 - [FIX] why initial position of selector + 1 is the same as +2?
         (the board starts printing at 0)
-- [FIX] delete structs, unnecessary?
+- [CODE] change everything to bool
+- [CODE] systematize iterator names
+- [CODE] check_for_errors non universal, but we'll see close to the end of project
+- [CODE] separate headers file?
 */
 
 #define TRUE 1
 #define FALSE 0
+
+/*
+#ifdef _WIN32
+    #define clear_screen system("cls")
+#elif __APPLE__
+    // printf ("\33c\e[3J");
+    #define clear_screen system("clear") // fake, only prints a bunch of \n
+#endif
+*/
 
 typedef struct game_parameters
 {
@@ -56,60 +68,67 @@ typedef struct selector
 
 typedef struct piece
 {
-	int alive; // 0 - dead, 1 - alive
-	int colour; // 0 - black, 1 - white
-	int type; // 0 - man, 1 - king
+	int alive;
+	int colour;
+	int type;
+
 	int pos_x;
 	int pos_y;
 }piece;
 
-// void clear_screen(); // use if running in standard windows terminals, unsafe (system()); replaced with printf ("\33c\e[3J") in main
-void fill_board(struct sizeof_board b1, char board[b1.rows][b1.columns]);
-void print_board(struct sizeof_board b1, char board[b1.rows][b1.columns]);
-void move_selector(struct sizeof_board b1, char board[b1.rows][b1.columns], struct selector *s1);
-void set_selector(struct sizeof_board b1, char board[b1.rows][b1.columns], struct selector s1);
-void clear_selector(struct sizeof_board b1, char board[b1.rows][b1.columns], struct selector *s1);
-void fill_men(struct sizeof_board b1, char board[b1.rows][b1.columns], piece p1[g1.number_of_pieces]);
-// void ask_for_dimensions(struct sizeof_board *b1); // just for debugging properties
+void fill_board(sizeof_board b1, char board[b1.rows][b1.columns]);
+void print_board(sizeof_board b1, char board[b1.rows][b1.columns]);
+void move_selector(sizeof_board b1, char board[b1.rows][b1.columns], selector *s1);
+void set_selector(sizeof_board b1, char board[b1.rows][b1.columns], selector s1);
+void clear_selector(sizeof_board b1, char board[b1.rows][b1.columns], selector *s1);
+void fill_men(sizeof_board b1, char board[b1.rows][b1.columns], game_parameters g1, piece p1[g1.number_of_pieces]);
+void print_men(sizeof_board b1, char board[b1.rows][b1.columns], game_parameters g1, piece p1[g1.number_of_pieces]);
+// void ask_for_dimensions(game_parameters *g1); // just for debugging properties
+void check_for_errors(game_parameters g1);
 
 int main()
 {
-    struct game_parameters g1 = {10, 30, 1};
+	// initial parameters
+
+    game_parameters g1 = {10, 30, 1};
+    check_for_errors(g1); // check if g1.number_of_pieces won't be too big for this g1.board_dimensions
     // ask_for_dimensions(&g1);
     int end_of_game = FALSE; // variable for end of the game
 
-    struct sizeof_board b1 = {10, 5, 6, 3, b1.square_height*g1.board_dimensions+b1.border_y+1, b1.square_width*g1.board_dimensions+b1.border_x+1}; // define board size with default dimensions 10x10
-    struct selector s1 = {b1.border_y+1, b1.border_x+1}; // define selector starting position
+
+    // defining game objects
+
+    sizeof_board b1 = {10, 5, 6, 3, b1.square_height*g1.board_dimensions+b1.border_y+1, b1.square_width*g1.board_dimensions+b1.border_x+1}; // define board size with default dimensions 10x10
+    selector s1 = {b1.border_y+1, b1.border_x+1}; // define selector starting position
     char board[b1.rows][b1.columns]; // create an array for board
-    struct piece p1[g1.number_of_pieces]; // create an array of structures for pieces
+    piece p1[g1.number_of_pieces]; // create an array of structures for pieces 
+
+
+    // filling game objects with initial content
 
     fill_board(b1, board); // fill board array with graphic symbols
+    fill_men(b1, board, g1, p1);
+
+
+    // gameplay
 
     while(!end_of_game)
     {
-        printf("\33c\e[3J"); // clear the console, replace with clear_screen if necessary
-        fill_men(b1, board, p1);
+        printf("\33c\e[3J"); // clear the console, replace with clear_screen; below if necessary
+        //clear_screen; // use if running in standard windows terminals, unsafe (system())
         set_selector(b1, board, s1); // input the selector symbols into the board array
         print_board(b1, board); // print the board array
         printf("%d %d", s1.i, s1.j); // debugging line
+        for (int i=0; i<g1.number_of_pieces/2; i++)
+        {
+        	printf("\n%d\n%d\n%d\n%d\n%d\n", p1[i].alive, p1[i].colour, p1[i].type, p1[i].pos_x, p1[i].pos_y); // debugging line
+        }
         move_selector(b1, board, &s1); // change selector position
     }
     return 0;
 }
 
-/*
-void clear_screen()
-{
-    #ifdef _WIN32
-        system("cls");
-    #elif __APPLE__
-        // printf ("\33c\e[3J");
-        system("clear"); // fake, only prints a bunch of \n
-    #endif
-}
-*/
-
-void fill_board(struct sizeof_board b1, char board[b1.rows][b1.columns])
+void fill_board(sizeof_board b1, char board[b1.rows][b1.columns])
 {
     // fill the board
 
@@ -122,7 +141,7 @@ void fill_board(struct sizeof_board b1, char board[b1.rows][b1.columns])
     	}
     }
 
-    for(int i=b1.border_y; i<b1.rows; i=i+b1.square_height) // every b1.square_height row starting from b1.border_y
+    for(int i=b1.border_y; i<b1.rows; i+=b1.square_height) // every b1.square_height row starting from b1.border_y
     {
     	for(int j=0; j<b1.border_x; j++)
     	{
@@ -135,7 +154,7 @@ void fill_board(struct sizeof_board b1, char board[b1.rows][b1.columns])
         }
     }
 
-    for(int i=b1.border_y+1; i<b1.rows; i=i+b1.square_height) // every b1.square_height row starting from b1.border_y
+    for(int i=b1.border_y+1; i<b1.rows; i+=b1.square_height) // every b1.square_height row starting from b1.border_y
     {
         for(int k=i; k<i+b1.square_height-1; k++) // go b1.square_height-1 rows at a time
         {
@@ -144,14 +163,14 @@ void fill_board(struct sizeof_board b1, char board[b1.rows][b1.columns])
     			board[k][j] = ' '; // fill the first b1.border_x cells with spaces
     		}
 
-            for(int j=b1.border_x; j<b1.columns; j=j+b1.square_width) // fill every b1.square_width cell starting from b1.border_x with vertical line
+            for(int j=b1.border_x; j<b1.columns; j+=b1.square_width) // fill every b1.square_width cell starting from b1.border_x with vertical line
             {
                 board[k][j] = '|';
             }
         }
     }
 
-    for(int i=b1.border_y+1; i<b1.rows; i=i+b1.square_height) // every b1.square_height row starting from b1.border_y+1
+    for(int i=b1.border_y+1; i<b1.rows; i+=b1.square_height) // every b1.square_height row starting from b1.border_y+1
     {
         for(int l=i; l<i+b1.square_height-1; l++) // go b1.square_height-1 rows at a time
         {
@@ -160,7 +179,7 @@ void fill_board(struct sizeof_board b1, char board[b1.rows][b1.columns])
     			board[l][k] = ' '; // fill the first b1.border_x cells with spaces
     		}
 
-            for(int j=b1.border_x+1; j<b1.columns; j = j+b1.square_width) // set j on every b1.square_width cell starting from b1.border_x+1
+            for(int j=b1.border_x+1; j<b1.columns; j+=b1.square_width) // set j on every b1.square_width cell starting from b1.border_x+1
             {          
                 for(int k=j; k<j+b1.square_width-1; k++) // fill b1.square_width-1 consecutive cells with space
                 {
@@ -172,7 +191,7 @@ void fill_board(struct sizeof_board b1, char board[b1.rows][b1.columns])
 
 }
 
-void print_board(struct sizeof_board b1, char board[b1.rows][b1.columns])
+void print_board(sizeof_board b1, char board[b1.rows][b1.columns])
 {
     // print the board
 
@@ -188,7 +207,7 @@ void print_board(struct sizeof_board b1, char board[b1.rows][b1.columns])
 
 }
 
-void move_selector(struct sizeof_board b1, char board[b1.rows][b1.columns], struct selector *s1)
+void move_selector(sizeof_board b1, char board[b1.rows][b1.columns], selector *s1)
 {
     char key = 0; // buffer for key pressed
 
@@ -256,7 +275,7 @@ void move_selector(struct sizeof_board b1, char board[b1.rows][b1.columns], stru
     }
 }
 
-void set_selector(struct sizeof_board b1, char board[b1.rows][b1.columns], struct selector s1)
+void set_selector(sizeof_board b1, char board[b1.rows][b1.columns], selector s1)
 {
     board[s1.i][s1.j] = '|'; // set upper left corner
     board[s1.i][s1.j+b1.square_width-2] = '|'; // set upper right corner b1.square_width-2 spaces to the right
@@ -283,7 +302,7 @@ void set_selector(struct sizeof_board b1, char board[b1.rows][b1.columns], struc
     */
 }
 
-void clear_selector(struct sizeof_board b1, char board[b1.rows][b1.columns], struct selector *s1)
+void clear_selector(sizeof_board b1, char board[b1.rows][b1.columns], selector *s1)
 {
     board[s1->i][s1->j] = ' '; // clear upper left corner
     board[s1->i][s1->j+b1.square_width-2] = ' '; // clear upper right corner x spaces to the right (now x = 3)
@@ -301,23 +320,50 @@ void clear_selector(struct sizeof_board b1, char board[b1.rows][b1.columns], str
     */
 }
 
-void fill_men(struct sizeof_board b1, char board[b1.rows][b1.columns], piece p1[g1.number_of_pieces])
+void fill_men(sizeof_board b1, char board[b1.rows][b1.columns], game_parameters g1, piece p1[g1.number_of_pieces])
+{
+	int k = 0;
+
+	for(int i=0; i<g1.number_of_pieces/2; i++) // define the starting parameters for first 15 pieces (black)
+	{
+		p1[i].alive = 1;
+		p1[i].colour = 0;
+		p1[i].type = 0;
+	}
+
+	/*
+	while(k<g1.number_of_pieces/2)
+	{
+		for(int i=0; i<g1.board_dimensions; i++) // go as many times as there are rows
+		{
+			for(int j=0; j<g1.board_dimensions; j++) // go as many times as there are columns
+			{
+				p1[k+j].pos_y = b1.border_y+1+i*b1.square_height;
+				p1[k+j].pos_x = b1.border_x+1+j*b1.square_width;
+				k += j;
+			}
+		}
+	}
+	*/
+}
+
+void print_men(sizeof_board b1, char board[b1.rows][b1.columns], game_parameters g1, piece p1[g1.number_of_pieces])
 {
     for(int i=b1.border_y+1; i<b1.rows; i=i+b1.square_height) // every b1.square_height row starting from b1.border_y+1
     {
-        for(int l=i; l<i+b1.square_height-1; l++) // go b1.square_height-1 rows at a time
-        {
+        //for(int l=i; l<i+b1.square_height-1; l++) // go b1.square_height-1 rows at a time
+        //{
             for(int k=b1.border_x+1; k<b1.columns; k = k+b1.square_width) // set j on every b1.square_width cell starting on b1.border_x+1
             {
-                board[l][k] = '0';
+
                 /*      
                 for(int k=j; k<j+b1.square_width-1; k++) // fill b1.square_width-1 consecutive cells with space
                 {
                     
                 }
                 */
-            }
-        }
+          	}
+      	//}
     }
 
     /*
@@ -327,6 +373,7 @@ void fill_men(struct sizeof_board b1, char board[b1.rows][b1.columns], piece p1[
     board[s1.i+b1.square_height-2][s1.j+b1.square_width-2] = '|'; // set bottom right corner b1.square_width-2 spaces to the right, b1.square_height-2 spaces down
     */
 }
+
 
 /*
 void ask_for_dimensions(game_parameters *g1)
@@ -348,3 +395,19 @@ void ask_for_dimensions(game_parameters *g1)
    }
 }
 */
+
+void check_for_errors(game_parameters g1)
+{
+	if(g1.number_of_pieces>(g1.board_dimensions*g1.board_dimensions)/2)
+    {
+    	printf("ERROR: Too big number_of_pieces for this board_dimensions, please change variable\nThe program will now terminate.\nPress [Enter] to continue...");
+    	getchar();
+    	exit(0);
+    }
+    else if(g1.number_of_pieces<0)
+    {
+    	printf("ERROR: Too small number_of_pieces for this board_dimensions, please change variable.\nThe program will now terminate.\nPress [Enter] to continue...");
+    	getchar();
+    	exit(0);
+    }
+}
